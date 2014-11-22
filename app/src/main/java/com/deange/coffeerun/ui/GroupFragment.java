9f9de2936e503e8b2960eadb62f46240536d1b6f
cdeange <christiandeange@gmail.com>
2014-11-21 19:18:33 -0500

    Early design for the main activity

---
+++ app/src/main/java/com/deange/coffeerun/ui/GroupFragment.java
@@ -0,0 +1,12 @@
+package com.deange.coffeerun.ui;
+
+import android.support.v4.app.Fragment;
+
+public class GroupFragment
+    extends Fragment {
+
+    public static Fragment newInstance(final int position) {
+        return new GroupFragment();
+    }
+
+}
--- app/src/main/java/com/deange/coffeerun/ui/MainActivity.java
+++ app/src/main/java/com/deange/coffeerun/ui/MainActivity.java
@@ -1,15 +1,94 @@
 package com.deange.coffeerun.ui;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity
-    extends FragmentActivity {
+import com.deange.coffeerun.R;
+
+public class MainActivity extends ActionBarActivity
+        implements NavigationDrawerFragment.OnDrawerItemSelectedListener {
+
+    private static final String FRAGMENT_TAG = "FRAGMENT";
+
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+    private String mTitle;
+    private Toolbar mToolbar;
 
     @Override
-    public void onCreate(final Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
 
+        findNavigationDrawer();
+
+        // Set up the drawer.
+        mNavigationDrawerFragment.setUp(
+                R.id.navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout),
+                mToolbar);
     }
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+
+        findNavigationDrawer();
+
+        if (mNavigationDrawerFragment != null) {
+            setTitle(String.valueOf(mNavigationDrawerFragment.getItem(position)));
+        }
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, GroupFragment.newInstance(position), FRAGMENT_TAG)
+                .commit();
+    }
+
+    private void findNavigationDrawer() {
+        mNavigationDrawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+    }
+
+    @Override
+    public void showGlobalContextActionBar() {
+        setTitle(R.string.app_name);
+    }
+
+    public void setTitle(final String title) {
+        mTitle = title;
+        refreshActionBar();
+    }
+
+    @Override
+    public void setTitle(final int titleId) {
+        mTitle = getString(titleId);
+        refreshActionBar();
+    }
+
+    public void refreshActionBar() {
+        if (mToolbar != null) {
+            mToolbar.setTitle(mTitle);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (!mNavigationDrawerFragment.isDrawerOpen()) {
+            refreshActionBar();
+            return true;
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
 }
