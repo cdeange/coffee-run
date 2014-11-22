6d79b5c947343fd583b38abec9dd92abbcd04bc5
cdeange <christiandeange@gmail.com>
2014-11-21 20:57:40 -0500

    Add navigation colour and floating action button

--- app/src/main/java/com/deange/coffeerun/ui/GroupFragment.java
+++ app/src/main/java/com/deange/coffeerun/ui/GroupFragment.java
@@ -1,12 +1,34 @@
 package com.deange.coffeerun.ui;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.deange.coffeerun.R;
+import com.melnykov.fab.FloatingActionButton;
 
 public class GroupFragment
     extends Fragment {
+
+    private FloatingActionButton mCheckinButton;
 
     public static Fragment newInstance(final int position) {
         return new GroupFragment();
     }
 
+    @Override
+    public View onCreateView(final LayoutInflater inflater, @Nullable final ViewGroup container,
+            @Nullable final Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(final View view, @Nullable final Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mCheckinButton = (FloatingActionButton) view.findViewById(R.id.floating_action_button);
+    }
 }
--- app/src/main/java/com/deange/coffeerun/ui/MainActivity.java
+++ app/src/main/java/com/deange/coffeerun/ui/MainActivity.java
@@ -3,7 +3,6 @@ package com.deange.coffeerun.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
